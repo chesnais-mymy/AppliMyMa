89afdc7a09613b7f5392e11e59d9a36a702d3b8d
David MA <ha_257@hotmail.com>
2015-12-31 23:20:26 +0100

    Ahlala ithub

--- app/src/main/java/org/esiea/chesnais_ma/applimyma/recapitulatif.java
+++ app/src/main/java/org/esiea/chesnais_ma/applimyma/recapitulatif.java
@@ -18,15 +18,6 @@ public class recapitulatif extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
         TextView cart = (TextView) findViewById(R.id.textView5);
         cart.setText(
                 "Votre récapitulatif est :" +
